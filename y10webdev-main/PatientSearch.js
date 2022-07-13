082e337ed9be3a5612c6045ac872137de788d4fc
Hamza Ammar <hamza.k.ammar@gmail.com>
2022-07-13 15:45:46 -0400

    Js

--- y10webdev-main/PatientSearch.js
+++ y10webdev-main/PatientSearch.js
@@ -1,3 +1,10 @@
-searchbar = document.getElementsByClassName("Search").value
+searchbar = document.getElementsByClassName("Search").value;
+
+document.getElementsByClassName("Name").value = sessionStorage.getItem("name");
+document.getElementsByClassName("Healthcard").value = sessionStorage.getItem("healthcard");
+document.getElementsByClassName("Age").value = sessionStorage.getItem("age");
+document.getElementsByClassName("DOB").value = sessionStorage.getItem("dateofbirth");
+document.getElementsByClassName("RFV").value = sessionStorage.getItem("reasonforvisit");
+document.getElementsByClassName("LOS").value = sessionStorage.getItem("lengthofvisit");
 
 
